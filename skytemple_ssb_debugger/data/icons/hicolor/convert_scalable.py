cdad7580f380e28d30c2f34c3c699cf773045a83
Parakoopa <parakoopa@live.de>
2020-06-12 23:40:52 +0200

    Use the GTK command for converting scalable SVGs to PNGs instead

--- skytemple_ssb_debugger/data/icons/hicolor/convert_scalable.py
+++ skytemple_ssb_debugger/data/icons/hicolor/convert_scalable.py
@@ -1,9 +1,10 @@
 #!/usr/bin/env python3
 # Converts the scalable icons into PNGs up to 512x512px.
+import os
 from glob import glob
 from os import system
 
 for in_path in glob('scalable/apps/*.svg'):
     for dim in [16, 32, 64, 128, 256, 512]:
-        out_path = in_path[:-4].replace('scalable/', f'{dim}x{dim}/') + '.png'
-        system(f'inkscape -w {dim} -h {dim} {in_path} --export-filename {out_path}')
+        out_path = os.path.dirname(in_path).replace('scalable/', f'{dim}x{dim}/')
+        system(f'gtk-encode-symbolic-svg {in_path} {dim}x{dim} -o {out_path}')
