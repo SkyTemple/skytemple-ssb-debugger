8a30c5c77b92bad2b99ae322123c760e71c75b93
Parakoopa <parakoopa@live.de>
2020-12-02 15:47:02 +0100

    Fixes #13: Link to ExplorerScript docs updated

--- skytemple_ssb_debugger/controller/main.py
+++ skytemple_ssb_debugger/controller/main.py
@@ -628,7 +628,7 @@ class MainController:
 
     # MENU HELP
     def on_menu_help_exps_docs_activate(self, btn: Gtk.MenuItem, *args):
-        webbrowser.open_new_tab("https://explorerscript.readthedocs.io/language_spec.html")
+        webbrowser.open_new_tab("https://explorerscript.readthedocs.io/en/latest/language_spec.html")
 
     def on_menu_help_about_activate(self, btn: Gtk.MenuItem, *args):
         self.builder.get_object("about_dialog").run()
