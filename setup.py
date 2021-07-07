b9b30034a31524705b4e1a81bc3d256560140113
Parakoopa <parakoopa@live.de>
2021-07-07 11:14:23 +0200

    skytemple-files update

--- setup.py
+++ setup.py
@@ -18,7 +18,7 @@ setup(
     url='https://github.com/SkyTemple/skytemple-ssb-debugger',
     install_requires=[
         'ndspy >= 3.0.0',
-        'skytemple-files >= 1.2.3',
+        'skytemple-files >= 1.2.4',
         'skytemple-icons >= 0.1.0',
         'pygobject >= 3.26.0',
         'pycairo >= 1.16.0',
