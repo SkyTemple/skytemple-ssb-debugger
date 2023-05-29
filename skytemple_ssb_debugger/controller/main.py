c21d4e3cb3ed444e34563c7d1a654ec23b1ce1ad
Capypara <hello@capypara.de>
2023-05-29 12:03:25 +0200

    Providing a way to find out if ov11 is loaded

--- skytemple_ssb_debugger/controller/main.py
+++ skytemple_ssb_debugger/controller/main.py
@@ -27,6 +27,7 @@ from typing import Optional, Dict, List
 import cairo
 import gi
 from PIL import Image
+from range_typed_integers import u32
 
 from skytemple_files.common.util import open_utf8, add_extension_if_missing
 from skytemple_ssb_emulator import SCREEN_WIDTH, SCREEN_HEIGHT, emulator_load_controls, Language, emulator_poll, \
@@ -1271,13 +1272,19 @@ class MainController:
     def emu_reset(self):
         if self.breakpoint_state:
             self.breakpoint_state.fail_hard()
+        loaded_overl_grp1_addr = 0
         if self.debugger and self.debugger.ground_engine_state:
-                self.debugger.ground_engine_state.reset(fully=True)
+            self.debugger.ground_engine_state.reset(fully=True)
+            assert self.debugger.rom_data is not None
+            loaded_overl_grp1_addr = self.debugger.rom_data.bin_sections.arm9.data.LOADED_OVERLAY_GROUP_1.absolute_address
         try:
             lang = self.settings.get_emulator_language()
             if lang:
                 emulator_set_language(lang)
-            emulator_open_rom(self.context.get_rom_filename())
+            emulator_open_rom(
+                self.context.get_rom_filename(),
+                u32(loaded_overl_grp1_addr)
+            )
             self.emu_is_running = emulator_is_running()
         except RuntimeError:
             self.context.display_error(
